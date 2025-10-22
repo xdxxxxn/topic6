1ec34b1594e015d78c2cae4052bc8f3b73815dd9
Барышева Виктория Вадимовна <admin@localhost.localdomain>
2025-10-22 17:21:05 +0700

    Добавлен метод getCarByBrendAndYersOperational

--- Main.java
+++ Main.java
@@ -8,13 +8,19 @@ public class Main {
         new Car(5, "BMW", "M5", 2022, "Красный", 85000.0, "B500BM")
     };
 
-    Car[] volvos = getCarByBrend(garage, "Volvo");
+    Car[] volvos = getCarByBrend(mass, "Volvo");
     System.out.println("Найдено Volvo: " + volvos.length);
-    Car[] bmws = getCarByBrend(garage, "BMW");
+    Car[] bmws = getCarByBrend(mass, "BMW");
     System.out.println("Найдено BMW: " + bmws.length);
-    Car[] astons = getCarByBrend(garage, "Aston Martin");
+    Car[] astons = getCarByBrend(mass, "Aston Martin");
     System.out.println("Найдено Aston Martin: " + astons.length);
 
+    Car[] oldVolvos = getCarByBrendAndYearOperational(mass, "Volvo", 1);
+    System.out.println("Volvo старше 1 года: " + oldVolvos.length);
+    Car[] oldBMWs = getCarByBrendAndYearOperational(mass, "BMW", 2);
+    System.out.println("BMW старше 2 лет: " + oldBMWs.length);
+    Car[] oldAstons = getCarByBrendAndYearOperational(mass, "Aston Martin", 0);
+    System.out.println("Aston Martin старше 0 лет: " + oldAstons.length);
   }
 
   public static Car[] getCarByBrend(Car[] cars, String brend) {
@@ -35,5 +41,31 @@ public class Main {
        }
      }
      return res;
+  }
+
+  public static Car[] getCarByBrendAndYearOperational(Car[] cars, String brend, int years) {
+
+    int cyear = 2025;
+
+    int count = 0;
+    for (int i = 0; i < cars.length; i++) {
+        int carAge = cyear - cars[i].getYear();
+        if (cars[i].getBrand().equals(brend) && carAge > years) {
+            count++;
+        }
+    }
+
+    Car[] result = new Car[count];
+
+    int in = 0;
+    for (int i = 0; i < cars.length; i++) {
+        int carAge = cyear - cars[i].getYear();
+        if (cars[i].getBrand().equals(brend) && carAge > years) {
+            result[in] = cars[i];
+            in++;
+        }
+    }
+    return result;
+  }
 }
 
