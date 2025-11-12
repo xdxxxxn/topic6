4542701d299e7445a9f64abb3075993a767ef467
Барышева Виктория Вадимовна <admin@localhost.localdomain>
2025-11-12 20:20:19 +0700

    добавлен метод exists

--- topic8/Cache.java
+++ topic8/Cache.java
@@ -22,4 +22,8 @@ public class Cache<T> {
     public boolean remove(T item) {
         return items.remove(item);
     }
+
+    public boolean exists(T item) {
+        return items.contains(item);
+    }
 }
